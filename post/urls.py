2446b387b15fcca3d6c1135489541c5da9e6e2b6
Iffat Hossain <iffathossain@iffats-air.mynetworksettings.com>
2022-05-30 14:44:48 -0400

    updates urls in post, and project level

--- post/urls.py
+++ post/urls.py
@@ -2,12 +2,16 @@ from django.urls import URLPattern, path
 from .views import PostViewSet
 from rest_framework import routers
 
+# router = routers.DefaultRouter()
+# router.register(r'post', PostViewSet)
+
 router = routers.DefaultRouter()
-router.register(r'post', PostViewSet)
+router.register(r'post', PostViewSet, basename='post')
+# urlpatterns = router.urls
 
-urlpatterns = [
-  path('post', PostViewSet.as_view()),
+# urlpatterns = [
+#   path('post', PostViewSet.as_view({'get': 'list'})),
  
-]
+# ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
